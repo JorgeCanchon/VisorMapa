70bcbe78cbcf41dc298d3e870a342b76bcf18e3a
JorgeCanchon <jlcanchon@misena.edu.co>
2018-03-01 11:55:41 -0500

    initial commit

---
+++ VisorMapa/App_Start/WebApiConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace VisorMapa
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Configuración y servicios de API web
+            //Politica de origen Install-Package Microsoft.AspNet.WebApi.Cors
+            config.EnableCors();
+            
+            // Rutas de API web
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "services/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
